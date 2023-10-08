573509d7c009382626d1cdd7eee6c73728677948
YukiHime23 <luutruhocit@gmail.com>
2023-10-09 00:50:18 +0700

    update sql

--- db/migration/000002_create_users_table.up.sql
+++ db/migration/000002_create_users_table.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE users (
-    id bigserial NOT NULL,
+    uid UUID NOT NULL DEFAULT pg_catalog.gen_random_uuid(),
     username text NULL,
-    email text NULL,
+    email text NOT NULL,
     "password" text NULL,
     created_at timestamptz NULL,
     updated_at timestamptz NULL,
     deleted_at timestamptz NULL,
     CONSTRAINT users_email_key UNIQUE (email),
-    CONSTRAINT users_pkey PRIMARY KEY (id)
+    CONSTRAINT users_pkey PRIMARY KEY (uid)
 );
 CREATE INDEX idx_users_deleted_at ON users USING btree (deleted_at);
